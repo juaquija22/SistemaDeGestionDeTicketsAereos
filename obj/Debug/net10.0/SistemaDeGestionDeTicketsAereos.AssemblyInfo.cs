dd8633a809342db8128bb113cc3193edecaf5421
Valentina Mancilla <valem2958@gmail.com>
2026-04-13 08:34:59 -0500

    feat: :sparkles: estructuracion de carpetas
    se estructrucraron las carpetas de una buena manera

--- obj/Debug/net10.0/SistemaDeGestionDeTicketsAereos.AssemblyInfo.cs
+++ obj/Debug/net10.0/SistemaDeGestionDeTicketsAereos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaDeGestionDeTicketsAereos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a3e37ecd0ead5e6e2a173314bd50bf632f52da3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccb2c88ff00ceecee1afb2c55494d992525014ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaDeGestionDeTicketsAereos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaDeGestionDeTicketsAereos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
